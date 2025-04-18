02dbc59242146bdd16ed6f3bd6c93eb4586bba99
SanudaKJ <sanudajayasinghe18@gmail.com>
2025-04-19 02:03:20 +0530

    feat: UI changes of auth pages

--- electricity_managemant_system/lib/pages/auth/login.dart
+++ electricity_managemant_system/lib/pages/auth/login.dart
@@ -1,5 +1,7 @@
 import 'package:electricity_managemant_system/pages/auth/register.dart';
 import 'package:electricity_managemant_system/pages/home/home.dart';
+import 'package:electricity_managemant_system/widgets/custom_widgets.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -13,124 +15,178 @@ class loginpage extends StatefulWidget {
 class _loginpageState extends State<loginpage> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+
+  Future<void> login() async {
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: emailController.text.trim(),
+          password: passwordController.text.trim());
+    } catch (e) {
+      print(e);
+      return;
+    }
+    if (mounted) {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => const home()));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: SingleChildScrollView(
-        child: Container(
-          constraints: BoxConstraints(
-            minHeight: MediaQuery.of(context).size.height,
-          ),
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // const Spacer(),
-              // const Text('Login Page'),
-              Image.asset(
-                'assets/logos/lightmode2.png',
-                height: 200,
-              ),
-
-              const SizedBox(height: 20),
-              Padding(
-                padding: const EdgeInsets.only(top: 20.0),
-                child: TextFormField(
-                  controller: emailController,
-                  decoration: const InputDecoration(
-                      contentPadding: EdgeInsets.all(8),
-                      hintText: 'Enter Your Email Address',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                        borderSide: BorderSide(color: Colors.blue),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10)),
-                          borderSide: BorderSide(
-                            color: Colors.blue,
-                          ))),
-                ),
-              ),
-              const SizedBox(height: 10),
-              Padding(
-                padding: const EdgeInsets.only(top: 10.0),
-                child: TextFormField(
-                  controller: passwordController,
-                  decoration: const InputDecoration(
-                    contentPadding: EdgeInsets.all(8),
-                    hintText: 'Enter Password',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                      borderSide: BorderSide(color: Colors.blue),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                        borderSide: BorderSide(
-                          color: Colors.blue,
-                        )),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 2),
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  TextButton(
-                    onPressed: () {},
-                    child: const Text(
-                      'Forgot Password?',
-                      style: TextStyle(
-                        color: Colors.black,
+        child: SingleChildScrollView(
+          child: Container(
+            constraints: BoxConstraints(
+              minHeight: MediaQuery.of(context).size.height,
+            ),
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Stack(
+                  children: [
+                    Container(
+                      height: 300, // Adjust height as needed
+                      width: double.infinity,
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/logos/lightmode2.png'),
+                          fit: BoxFit.cover,
+                        ),
                       ),
                     ),
-                  )
-                ],
-              ),
-              // Login Button
-              SizedBox(
-                width: double.infinity,
-                height: 50,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => home()));
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.black,
-                  ),
-                  child: const Text(
-                    'Login',
-                    style: TextStyle(color: Colors.white),
-                  ),
+                  ],
                 ),
-              ),
-              // const Divider(),
-              // const Spacer(),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text('Don\'t have an account?'),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => Register()));
-                    },
-                    child: const Text(
-                      'Register',
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Welcome !',
                       style: TextStyle(
+                        fontSize: 25,
                         fontWeight: FontWeight.bold,
                         color: Colors.black,
                       ),
                     ),
-                  )
-                ],
-              )
-            ],
+                  ],
+                ),
+                const SizedBox(height: 10),
+                CustomTextField(
+                  controller: emailController,
+                  hintText: 'Email Address',
+                ),
+                const SizedBox(height: 10),
+                CustomTextField(
+                  controller: passwordController,
+                  hintText: 'Password',
+                  isPassword: true,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 0),
+                      child: TextButton(
+                        onPressed: () {},
+                        child: const Text(
+                          'Forgot Password?',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                CustomButton(
+                  text: 'Login',
+                  onPressed: login,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text('Don\'t have an account?'),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => Register()),
+                        );
+                      },
+                      child: const Text(
+                        'Register',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 0),
+                const Divider(
+                  thickness: 1,
+                  color: Colors.grey,
+                ),
+                const SizedBox(height: 0),
+                const Text(
+                  'Or continue with',
+                  style: TextStyle(color: Colors.grey),
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // Google Login Button
+                    ElevatedButton(
+                      onPressed: () {
+                        // Add Google login logic here
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        foregroundColor: Colors.black,
+                        side: const BorderSide(color: Colors.grey),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        padding: const EdgeInsets.all(
+                            10), // Adjust padding for icon size
+                      ),
+                      child: const Icon(
+                        Icons.apple,
+                        size: 24,
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    // Apple Login Button
+                    ElevatedButton(
+                      onPressed: () {
+                        // Add Apple login logic here
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.black,
+                        foregroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        padding: const EdgeInsets.all(
+                            10), // Adjust padding for icon size
+                      ),
+                      child: const Icon(
+                        Icons.apple,
+                        size: 24,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
-      )),
+      ),
     );
   }
 }
--- electricity_managemant_system/lib/pages/auth/register.dart
+++ electricity_managemant_system/lib/pages/auth/register.dart
@@ -1,4 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:electricity_managemant_system/pages/auth/login.dart';
+import 'package:electricity_managemant_system/widgets/custom_widgets.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -9,9 +13,38 @@ class Register extends StatefulWidget {
   State<Register> createState() => _RegisterState();
 }
 
+bool isChecked = false;
+
 class _RegisterState extends State<Register> {
+  final companyNameController = TextEditingController();
+  final passwordController = TextEditingController();
+  final emailController = TextEditingController();
+  final confirmpasswordController = TextEditingController();
   final List<String> companyTypes = ['Type 1', 'Type 2', 'Type 3'];
   String? selectedCompanyType;
+
+  Future<void> register() async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+              email: emailController.text.trim(),
+              password: passwordController.text.trim());
+      String userId = userCredential.user!.uid;
+      await FirebaseFirestore.instance.collection('users').doc(userId).set({
+        'companyName': companyNameController.text.trim(),
+        'companyType': selectedCompanyType,
+        'email': emailController.text.trim(),
+        'userId': userId,
+      });
+      if (mounted) {
+        Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) => const loginpage()));
+      }
+    } catch (e) {
+      print("Error: ${e.toString()}");
+      return;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,39 +57,80 @@ class _RegisterState extends State<Register> {
           ),
           padding: const EdgeInsets.all(20.0),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              // const Spacer(),
-              // const Text('Login Page'),
-              Image.asset(
-                'assets/logos/lightmode2.png',
-                height: 200,
-              ),
-
-              const SizedBox(height: 10),
-              Padding(
-                padding: const EdgeInsets.only(top: 20.0),
-                child: TextFormField(
-                  // controller: emailController,
-                  decoration: const InputDecoration(
-                      contentPadding: EdgeInsets.all(8),
-                      hintText: 'Enter Company Name',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                        borderSide: BorderSide(color: Colors.blue),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10)),
-                          borderSide: BorderSide(
-                            color: Colors.blue,
-                          ))),
-                ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'Sign Up',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
               ),
               const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'Create an account to get started',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'Company Name',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
+              CustomTextField(
+                controller: emailController,
+                hintText: 'Company Name',
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'Company Type',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
               Padding(
                 padding: const EdgeInsets.only(top: 10.0),
                 child: DropdownButtonFormField<String>(
-                  style: const TextStyle(color: Colors.black),
+                  style: const TextStyle(
+                    color: Colors.black, // Text color inside dropdown
+                    fontSize: 16, // Font size
+                    // Font weight
+                  ),
+                  icon: const Icon(
+                    Icons.arrow_drop_down, // Dropdown icon
+                    color: Colors.grey, // Icon color
+                  ),
                   value: selectedCompanyType,
                   items: companyTypes.map((String type) {
                     return DropdownMenuItem<String>(
@@ -69,115 +143,94 @@ class _RegisterState extends State<Register> {
                       selectedCompanyType = newValue;
                     });
                   },
-                  decoration: const InputDecoration(
-                    contentPadding: EdgeInsets.all(8),
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.all(8),
                     hintText: 'Select Company Type',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                      borderSide: BorderSide(color: Colors.blue),
+                    hintStyle: const TextStyle(
+                      color: Colors.grey, // Hint text color
                     ),
-                    focusedBorder: OutlineInputBorder(
+                    border: const OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                    focusedBorder: const OutlineInputBorder(
                       borderRadius: BorderRadius.all(Radius.circular(10)),
                       borderSide: BorderSide(
-                        color: Colors.blue,
+                        color: Colors.orange, // Border color when focused
                       ),
                     ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 10),
-              Padding(
-                padding: const EdgeInsets.only(top: 10.0),
-                child: TextFormField(
-                  // controller: emailController,
-                  decoration: const InputDecoration(
-                      contentPadding: EdgeInsets.all(8),
-                      hintText: 'Enter Your Email Address',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                        borderSide: BorderSide(color: Colors.blue),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10)),
-                          borderSide: BorderSide(
-                            color: Colors.blue,
-                          ))),
-                ),
-              ),
-              const SizedBox(height: 10),
-              Padding(
-                padding: const EdgeInsets.only(top: 10.0),
-                child: TextFormField(
-                  // controller: passwordController,
-                  decoration: const InputDecoration(
-                    contentPadding: EdgeInsets.all(8),
-                    hintText: 'Enter Password',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                      borderSide: BorderSide(color: Colors.blue),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                        borderSide: BorderSide(
-                          color: Colors.blue,
-                        )),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 10),
-              Padding(
-                padding: const EdgeInsets.only(top: 10.0),
-                child: TextFormField(
-                  // controller: passwordController,
-                  decoration: const InputDecoration(
-                    contentPadding: EdgeInsets.all(8),
-                    hintText: 'Confirm Password',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                      borderSide: BorderSide(color: Colors.blue),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                        borderSide: BorderSide(
-                          color: Colors.blue,
-                        )),
+
+                    // Background color of the dropdown field
                   ),
                 ),
               ),
               const SizedBox(height: 20),
-
-              // Row(
-              //   mainAxisAlignment: MainAxisAlignment.end,
-              //   children: [
-              //     TextButton(
-              //       onPressed: () {},
-              //       child: const Text(
-              //         'Forgot Password?',
-              //         style: TextStyle(
-              //           color: Colors.black,
-              //         ),
-              //       ),
-              //     )
-              //   ],
-              // ),
-              // Login Button
-              SizedBox(
-                width: double.infinity,
-                height: 50,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.black,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'Email Address',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
                   ),
-                  child: const Text(
-                    'Create Account',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
+                ],
               ),
-              // const Divider(),
-              // const Spacer(),
-
+              CustomTextField(
+                controller: emailController,
+                hintText: 'Email Address',
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'Password',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
+              CustomTextField(
+                controller: emailController,
+                hintText: 'Create a Password',
+              ),
+              CustomTextField(
+                controller: emailController,
+                hintText: 'Confirm Password',
+              ),
+              const SizedBox(height: 15),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Checkbox(
+                    value: isChecked,
+                    onChanged: (bool? value) {
+                      setState(() {
+                        isChecked = value ?? false;
+                      });
+                    },
+                    activeColor: Colors.orange, // Checkbox color
+                  ),
+                  const Text(
+                    'I agree to the Terms and Conditions',
+                    style: TextStyle(fontSize: 14, color: Colors.black),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 50),
+              CustomButton(
+                text: 'Create Account',
+                onPressed: () {
+                  register();
+                },
+              ),
+              const SizedBox(height: 5),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
